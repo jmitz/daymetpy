b180c7e3907df74252ee3270468a768036dc4467
Colin Talbert <talbertc@usgs.gov>
2016-03-15 10:31:54 -0600

    Update test to new package structure

--- tests/test_timeseries.py
+++ tests/test_timeseries.py
@@ -2,8 +2,8 @@ import unittest
 from datetime import datetime, timedelta
 
 import sys
-sys.path.append(r"..")
-from daymetpy import download_Daymet
+sys.path.append(r"../..")
+from daymetpy import daymet_timeseries
 
 class TimeseriesTest(unittest.TestCase):
 
@@ -12,7 +12,7 @@ class TimeseriesTest(unittest.TestCase):
 
     def test_ornl_df(self):
         ornl_lat, ornl_long = 35.9313167, -84.3104124
-        df = download_Daymet(lon=ornl_long, lat=ornl_lat, start_yr=2012, end_yr=2013)
+        df = daymet_timeseries(lon=ornl_long, lat=ornl_lat, start_year=2012, end_year=2012)
 
         self.assertTrue(df.year.count() == 365)
         self.assertTrue("tmax" in df.columns)
@@ -22,7 +22,7 @@ class TimeseriesTest(unittest.TestCase):
     def test_out_of_bounds(self):
         london_lat, london_long = 51.5072, 0.1275
         with self.assertRaises(NameError):
-            df = download_Daymet(lon=london_long, lat=london_lat, start_yr=2012, end_yr=2013)
+            df = daymet_timeseries(lon=london_long, lat=london_lat, start_year=2012, end_year=2012)
 
 
 if __name__ == '__main__':
